d57ad6ae9543934b0f0b7d6250f71c5997ad2975
neilmbennett <nmbennet@uci.edu>
2020-04-21 17:34:08 -0400

    gitignore file?

---
+++ .gitignore/credentials.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 17:21:38 2020
+
+@author: Neil
+
+Credential Keys for Aquarium
+"""
+
+CONSUMER_KEY = 'QaEOr85un66XhQMKYzNQYvrgO'
+CONSUMER_SECRET = 'W1xXaB7Ay1TJg4IGKdTQHAVj9SfawnWFdbuLr5uhx1RYfn4Cp3'
+ACCESS_KEY = '1252328806234361862-Xa6N1WPqToWJo5V3FoRTYcsLyAxWQs'
+ACCESS_SECRET = 'Ao4gVO3woanc8XL6xNn3IzEwCs1QkU5wPuo0yIZ7iUmKv'
